ab730badff5c0e0fe88b8c930972baaef092c017
FortyHives <157342124+FortyHives@users.noreply.github.com>
2024-06-26 14:17:17 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  return $request->user();
+});
 
 
 
