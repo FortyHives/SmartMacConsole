b0c5bdeac371adbac9f6f7b47d86177ae1ce24ff
FortyHives <157342124+FortyHives@users.noreply.github.com>
2024-06-26 12:08:00 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,10 +8,7 @@ use App\Http\Middleware\LocaleMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
   ->withRouting(
     web: __DIR__ . '/../routes/web.php',
-<<<<<<< HEAD
-=======
     api: __DIR__ . '/../routes/api.php', // Add this if not added by install:api
->>>>>>> parent of ea68716 (Update API routes)
     commands: __DIR__ . '/../routes/console.php',
     health: '/up',
   )
